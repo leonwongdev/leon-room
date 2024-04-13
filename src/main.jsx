1b5b327c9213d6ad05fc077f4f81df1d039ca6be
Lap Wang Wong <leonwonglww@gmail.com>
2024-04-13 12:54:29 -0400

    added helmet

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Helmet } from "react-helmet";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <App />
+  <>
+    <Helmet>
+      <title>{"Leon's Room"}</title>
+      <meta name="description" content="Leon's Portfolio" />
+      <meta property="og:title" content="Leon's Portfolio" />
+      <meta property="og:description" content="Leon's Portfolio" />
+      <meta property="og:image" content="/public/thumbnail.png" />
+    </Helmet>
+    <App />
+  </>
+
   // </React.StrictMode>,
 );
