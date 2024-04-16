f8df13bcc68fba41916022c5b71a7341d24decc2
Lap Wang Wong <leonwonglww@gmail.com>
2024-04-16 19:42:51 -0400

    Added projects

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,8 @@ function App() {
 
   // Skills
   const [skills, setSkills] = useState();
+  const [projects, setProjects] = useState();
+
   // Function to fetch data
   const fetchSkills = async () => {
     try {
@@ -45,8 +47,26 @@ function App() {
       console.debug(error);
     }
   };
+
+  const fetchProjects = async () => {
+    try {
+      const response = await fetch(
+        "https://portfolio-express-server-pi.vercel.app/api/projects"
+      ); // Replace with your API URL
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const result = await response.json();
+      console.debug("fetch projects payload=", result);
+      setProjects(result);
+    } catch (error) {
+      console.debug(error);
+    }
+  };
+
   useEffect(() => {
     fetchSkills();
+    fetchProjects();
   }, []);
 
   function onSplineLoad(spline) {
@@ -164,6 +184,69 @@ function App() {
     );
   }
 
+  function renderProjectsCarousel(projects) {
+    if (!projects || projects.length === 0) {
+      console.debug("renderProjectsCarousel:", "no projects");
+      return;
+    }
+    console.debug("renderProjectsCarousel: rendering project carousel");
+    return (
+      <div className="carousel space-x-4 w-full h-5/6">
+        {projects.map((project, index) => (
+          <div
+            key={project._id}
+            id={`slide${index + 1}`}
+            className="carousel-item relative w-full flex-col gap-2"
+          >
+            <p>{project.name}</p>
+            <div className="flex gap-1">
+              {project.tags.map((tag) => (
+                <span key={tag} className="badge badge-primary">
+                  {tag}
+                </span>
+              ))}
+            </div>
+
+            <div className="mockup-window border bg-blue-900 w-full">
+              <div className="flex justify-center bg-base-200 h-full">
+                <img
+                  src={project.imageUrl}
+                  className="w-full h-full object-contain"
+                  alt={`${project.name} Thumbnail`}
+                />
+              </div>
+            </div>
+            <div className="flex justify-center items-center">
+              <a
+                className="btn btn-outline btn-primary w-2/5"
+                href={project.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Live Demo
+              </a>
+            </div>
+
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a
+                href={`#slide${index === 0 ? projects.length : index}`}
+                className="btn btn-circle"
+              >
+                ❮
+              </a>
+              <a
+                href={`#slide${index === projects.length - 1 ? 1 : index + 2}`}
+                className="btn btn-circle"
+              >
+                ❯
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <>
       {renderLoading()}
@@ -209,126 +292,7 @@ function App() {
           <h3 className="font-bold text-lg mb-2">My Projects</h3>
 
           {/* sm:h-96 lg:h-[30rem] */}
-          <div className="carousel space-x-4 w-full h-5/6">
-            <div
-              id="slide1"
-              className="carousel-item relative w-full flex-col gap-2"
-            >
-              <p>Youtube Note-taking App</p>
-              <div className="flex gap-1">
-                <span className="badge badge-primary">React.js</span>
-                <span className="badge badge-primary">.Net 8</span>
-                <span className="badge badge-primary">T-SQL</span>
-              </div>
-
-              <div className="mockup-window border bg-blue-900 w-full">
-                <div className="flex justify-center bg-base-200 h-full">
-                  <img
-                    src={youtubeNoteTakingAppThumbnail}
-                    className="w-full h-full object-contain"
-                    alt="Youtube Note-taking App Thumbnail"
-                  />
-                </div>
-              </div>
-              <div className="flex justify-center items-center">
-                <a
-                  className="btn btn-outline btn-primary w-2/5"
-                  href="https://youtubenotes.netlify.app/"
-                  target="_blank"
-                >
-                  Live Demo
-                </a>
-              </div>
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide4" className="btn btn-circle ">
-                  ❮
-                </a>
-                <a href="#slide2" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-            {/* Slide 2 */}
-            <div
-              id="slide2"
-              className="carousel-item relative w-full flex-col gap-2"
-            >
-              <p>Hackathon Team Builder App</p>
-              <div className="flex gap-1">
-                <span className="badge badge-primary">.Net MVC 5</span>
-                <span className="badge badge-primary">.Net WebAPI</span>
-                <span className="badge badge-primary">EF 6</span>
-              </div>
-              <div className="mockup-window border bg-blue-900 w-full">
-                <div className="flex justify-center bg-base-200 h-full">
-                  <img
-                    src={hackathonTeamBuilderThumbnail}
-                    className="w-full h-full object-contain"
-                    alt="Hackathon Team Builder App Thumbnail"
-                  />
-                </div>
-              </div>
-              <div className="flex justify-center items-center">
-                <a
-                  className="btn btn-outline btn-primary w-2/5"
-                  href="https://hackathonteambuilder20240320220041.azurewebsites.net/"
-                  target="_blank"
-                >
-                  Live Demo
-                </a>
-              </div>
-
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide1" className="btn btn-circle ">
-                  ❮
-                </a>
-                <a href="#slide3" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-            {/* Slide 3 */}
-            <div
-              id="slide3"
-              className="carousel-item relative w-full flex-col gap-2"
-            >
-              <p>
-                Dictionary Chrome Extension {"(4.3 Stars / 75 Active Users)"}
-              </p>
-              <div className="flex gap-1">
-                <span className="badge badge-primary">HTML</span>
-                <span className="badge badge-primary">CSS</span>
-                <span className="badge badge-primary">JavaScript</span>
-              </div>
-              <div className="mockup-window border bg-blue-900 w-full">
-                <div className="flex justify-center bg-base-200 h-full">
-                  <img
-                    src={chromeExtensionThumbnail}
-                    className="w-full h-full object-contain"
-                    alt="Dictionary Chrome Extension Thumbnail"
-                  />
-                </div>
-              </div>
-              <div className="flex justify-center items-center">
-                <a
-                  className="btn btn-outline btn-primary w-2/5"
-                  href="https://chromewebstore.google.com/detail/dictionary-search-diction/clhkbghopdclagfplhknlhcpnjefpacd?hl=en&authuser=0"
-                  target="_blank"
-                >
-                  Get Extension
-                </a>
-              </div>
-
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide2" className="btn btn-circle ">
-                  ❮
-                </a>
-                {/* <a href="#slide4" className="btn btn-circle">
-                  ❯
-                </a> */}
-              </div>
-            </div>
-          </div>
+          {renderProjectsCarousel(projects)}
         </div>
       </dialog>
     </>
